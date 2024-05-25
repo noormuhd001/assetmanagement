429bd7fbf61ee6fc00ea5d14cb7f929a5aca3a73
Noor Muhammed <noor.bytekat@gmail.com>
2024-05-25 11:01:15 +0530

    removed gaps and unwanted comments in notofication folder pages

--- resources/views/notification/show.blade.php
+++ resources/views/notification/show.blade.php
@@ -1,54 +1,47 @@
 @extends('layout.layout')
-
 @section('content')
-
 @section('breadcrumbs')
     {{ Breadcrumbs::render('employee.notification') }}
 @endsection
-
-
 <div class="container">
     <div class="row">
         @foreach ($notification as $notifications)
             <div class="col-12 mb-3">
                 <div class="notification">
                     <strong>message:</strong> {{ $notifications->data }}
-                
                     <p> {{ $notifications->type }}</p>
                     <a href="{{ route('ticket.show', ['id' => $notifications->ticket_id]) }}">
-                        <button class="btn btn-primary" onclick="markAsRead({{ $notifications->id }})">Mark as Read</button>
-                    </a>    
-            </div>
+                        <button class="btn btn-primary" onclick="markAsRead({{ $notifications->id }})">Mark as
+                            Read</button>
+                    </a>
+                </div>
             </div>
         @endforeach
     </div>
 </div>
-
 <script>
     function markAsRead(notificationId) {
         const url = `/notifications/${notificationId}/read`;
         console.log("URL:", url); // Debug the URL
 
         fetch(url, {
-            method: 'PATCH',
-            headers: {
-                'X-CSRF-TOKEN': '{{ csrf_token() }}',
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => {
-            console.log("Response:", response); // Debug the response
-            return response.json();
-        })
-        .then(data => {
-            console.log("Data:", data); // Debug the data
-            if (data.message === 'Notification marked as read.') {
-                location.reload(); // Refresh the page
-            }
-        })
-        .catch(error => console.error('Error:', error));
+                method: 'PATCH',
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => {
+                console.log("Response:", response); // Debug the response
+                return response.json();
+            })
+            .then(data => {
+                console.log("Data:", data); // Debug the data
+                if (data.message === 'Notification marked as read.') {
+                    location.reload(); // Refresh the page
+                }
+            })
+            .catch(error => console.error('Error:', error));
     }
 </script>
-
-
 @endsection
--- resources/views/notification/store.blade.php
+++ resources/views/notification/store.blade.php
@@ -1,18 +1,8 @@
 @extends('layout.layout')
-
 @section('content')
-
-
 @section('breadcrumbs')
-
 {{ Breadcrumbs::render('notification.index') }}
 @endsection
-
-
     <h1>Notification Sent</h1>
     <p>{{ $messages['message'] }}</p>
-    {{-- <p>{{ $messages['wish'] }}</p> --}}
-
-
-
 @endsection

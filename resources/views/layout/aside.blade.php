20d993b9d9ae0ef76b4d2113f0718060328566b0
Noor Muhammed <noor.bytekat@gmail.com>
2024-05-27 09:59:34 +0530

    edited aside layout

--- resources/views/layout/aside.blade.php
+++ resources/views/layout/aside.blade.php
@@ -1,5 +1,5 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4 sidebar-visible">
-    <a href="index3.html" class="brand-link">
+    <a href="#" class="brand-link">
         <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">AssetTrack</span>
